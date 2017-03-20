6abeeee18dfda9257ab449408663d9bcdf76e52a
borsch <oleh.kurpiak@gmail.com>
2017-03-20 09:08:57 +0200

    Remove close button

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -12,24 +12,17 @@ USING_NS_CC;
 
 Scene* GameScene::createScene()
 {
-    // 'scene' is an autorelease object
 	auto scene = Scene::createWithPhysics();
     
-    // 'layer' is an autorelease object
 	auto layer = GameScene::create();
 
-    // add layer as a child to scene
     scene->addChild(layer, 0, "GameScene");
 
-    // return the scene
     return scene;
 }
 
-// on "init" you need to initialize your instance
 bool GameScene::init()
 {
-    //////////////////////////////
-    // 1. super init first
     if ( !Layer::init() )
     {
         return false;
@@ -38,26 +31,6 @@ bool GameScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-										   CC_CALLBACK_1(GameScene::menuCloseCallback, this));
-    
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
 	PhysicsShapeCache::getInstance()->addShapesWithFile(PHYSICS_PLIST);
 
 	_hero = Hero::create();
@@ -144,19 +117,11 @@ void GameScene::update(float dt)
 
 void GameScene::menuCloseCallback(Ref* pSender)
 {
-    //Close the cocos2d-x game scene and quit the application
     Director::getInstance()->end();
 
     #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 	#endif
-    
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-    
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-    
-    
 }
 
 void GameScene::setScore(float score)
@@ -183,6 +148,5 @@ void GameScene::setRunning(bool b)
 void GameScene::onEnterTransitionDidFinish()
 {
 	Layer::onEnterTransitionDidFinish();
-	// to run onUpdate() method
 	this->scheduleUpdate();
 }
